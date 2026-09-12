8db4c5a8736657b01698d524526e8a2067502974
Alexander Lyakhov <ajlyakhov@users.noreply.github.com>
2026-09-12 18:59:18 +0200

    fix: proto constructor chrome helpers

--- packages/ui/src/components/domain/page-body.tsx
+++ packages/ui/src/components/domain/page-body.tsx
@@ -10,7 +10,7 @@ export function PageBody({ className, children, ...props }: PageBodyProps) {
     <div
       data-slot="page-body"
       className={cn(
-        "mx-auto flex w-full flex-col gap-5",
+        "mx-auto flex w-full flex-col",
         "max-w-[1180px] px-8 pb-12 pt-7",
         "group-data-[shell-mode=tablet]/shell:max-w-[920px] group-data-[shell-mode=tablet]/shell:px-7 group-data-[shell-mode=tablet]/shell:pb-10 group-data-[shell-mode=tablet]/shell:pt-6",
         "group-data-[shell-mode=phone]/shell:max-w-full group-data-[shell-mode=phone]/shell:px-3.5 group-data-[shell-mode=phone]/shell:pb-8 group-data-[shell-mode=phone]/shell:pt-4",
